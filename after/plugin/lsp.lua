1a0d2ebcc677703a568a908f70c4d2bab243a1ef
ManuLasker <emanuelule2828@gmail.com>
2022-12-29 22:07:15 -0500

    automagic messaging from me in the past.  Have you accomplished something meaningful yet?

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -79,10 +79,10 @@ tabnine:setup({
 local cmp = require('cmp')
 local lspkind = require("lspkind")
 
---local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = {behavior = cmp.SelectBehavior.Insert}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    --['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    --['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
