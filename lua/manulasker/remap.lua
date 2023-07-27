eee90258edf027e4e514483e96e1c32c54baa605
ManuLasker <emanuelule2828@gmail.com>
2023-07-27 13:28:10 -0500

    automagic messaging from me in the past.  Have you accomplished something meaningful yet?

--- lua/manulasker/remap.lua
+++ lua/manulasker/remap.lua
@@ -36,7 +36,7 @@ vim.keymap.set("n", "<leader>*", ":%s/\\<<C-r><C-w>\\>//g<left><left>")
 vim.keymap.set("n", "<leader>&", ":%s/\\<<C-r><C-w>\\>//gc<left><left><left>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !source $HOME/.zshrc && tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
