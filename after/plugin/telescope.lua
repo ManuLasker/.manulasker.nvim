93d34be52efdf0c006d1d7d08f47de1dc5076408
ManuLasker <emanuelule2828@gmail.com>
2023-08-18 15:08:13 -0500

    automagic messaging from me in the past.  Have you accomplished something meaningful yet?

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,20 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+local find_current_session_dir = function()
+    if #vim.fn.argv() > 0 then
+        return vim.fn.argv()[1]
+    end
+    return vim.fn.getcwd()
+end
+
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files(
+        { hidden = true, cwd = find_current_session_dir() })
+end, {})
+
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
