adce39f69618fe856030a64756a926a99db46e8c
ManuLasker <emanuelule2828@gmail.com>
2023-10-08 23:10:37 -0500

    automagic messaging from me in the past.  Have you accomplished something meaningful yet?

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,14 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files(
+        { hidden = true, no_ignore = true, no_ignore_parent = true } )
+end, {})
+
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
