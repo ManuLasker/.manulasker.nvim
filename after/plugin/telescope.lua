b2819dc0d4e6a0079c513b3f077aacc0d289a0a1
ManuLasker <emanuelule2828@gmail.com>
2023-10-08 23:13:25 -0500

    automagic messaging from me in the past.  Have you accomplished something meaningful yet?

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,7 +3,7 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', function()
     builtin.find_files(
-        { hidden = true, no_ignore = true, no_ignore_parent = true } )
+        { hidden = false, no_ignore = true, no_ignore_parent = true } )
 end, {})
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
